ce37a17db20a8a9b45eeecbd1de3ea084f8822e8
Sergen Temel <tmlsergen@gmail.com>
2019-05-09 17:20:18 +0300

    Bug fix

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::post('register', 'Api\AuthController@register');
     Route::post('login', 'Api\AuthController@login');
-    Route::group(['middleware' => ['auth:api' , 'role']], function () {
-       Route::resource('/post', 'Api\PostController');
-       Route::resource('/comment', 'Api\CommentController');
-       Route::resource('/category', 'Api\CategoryController');
+    Route::get('/post', 'Api\PostController@index')->name('post.index');
+    Route::get('/post/{id}', 'Api\PostController@show')->name('post.show');
+    Route::resource('/category', 'Api\CategoryController');
+    Route::group(['middleware' => ['auth:api', 'role']], function () {
+        Route::resource('/comment', 'Api\CommentController')->except('show', 'index', 'edit', 'create');;
+        Route::resource('/post', 'Api\PostController')->except('show', 'index', 'edit', 'create');
     });
 });
